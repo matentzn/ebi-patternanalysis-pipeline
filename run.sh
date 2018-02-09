8f6b1323d45242c6e43acbfc2916312b972bcd74
matentzn <nicolas.matentzoglu@gmail.com>
2018-02-09 08:22:54 +0000

    Excluded inference analysis for now. HermiT too slow

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ echo '** running corpus debugger against corpus **'
 java $java_params -jar corpusdebugger.jar ${CORPUS} i 1 2 elk ${DATAOUT}
 
 echo '** running inference analysis against corpus **'
-#echo "Excluded due to memory requirements"
-java  $java_params -jar inferenceanalysis.jar ${CORPUS} i ${DATAOUT}
+echo "Excluded due to memory requirements"
+#java  $java_params -jar inferenceanalysis.jar ${CORPUS} i ${DATAOUT}
 
 echo '** running patternextract against corpus **'
 java  $java_params -jar patternextract.jar ${CORPUS} ${DATA}/branches.txt is ${DATAOUT}
